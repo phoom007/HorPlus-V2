b588140a023e7fb743ea823a478162ebee7de494
phoom007 <phoomgamertv@gmail.com>
2026-08-02 20:53:09 +0700

    test(wave1d): track shared backend test setup

---
+++ server/tests/setup.ts
@@ -0,0 +1,9 @@
+import { afterAll } from 'vitest';
+import { disconnectRedis } from '../src/db/redis.js';
+import { getPrismaClient } from '../src/db/prisma.js';
+
+afterAll(async () => {
+  await disconnectRedis();
+  const prisma = getPrismaClient();
+  await prisma.$disconnect();
+});
